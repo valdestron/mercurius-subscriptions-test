2bdeae59b3a7422881df3e709477a34242ce86f9
valdestron <valdas.mazrimas@gmail.com>
2021-03-01 11:21:06 +0200

    remove tests

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -6,25 +6,10 @@ import env from './utils/env'
 import logger from './utils/logger'
 import { WorkflowsSubscriptionResolver } from './resolvers/WorkflowsSubscription'
 import { initPubSub } from './connectors/pubsub'
-// import { MercuriusContext } from 'mercurius'
-// import { setupAuth0Clients, installAuth0Apps, installAuth0Roles } from '@libs/integrate-auth0'
 export type ServiceContext = FastifyRequest
 
 const start = async () => {
   try {
-    // TODO: this runs one time during migration only
-    // await setupAuth0Clients({
-    //   clientId: '5CrcRF5dwtRk1ScjhRRrG4vSbQko571I',
-    //   clientSecret: 'IWLEDMZ-6epLMLy5-HiunPZVC08hRIv2DveRneU04tu-gsaU1S3NgY2x423FjBiC',
-    //   domain: 'dev-2whak4bt.eu.auth0.com',
-    // })
-    // await installAuth0Apps({
-    //   callbacks: ['http://localhost:3000', 'http://localhost:3000/login'],
-    //   allowed_logout_urls: ['http://localhost:3000', 'http://localhost:3000/logout'],
-    //   web_origins: ['http://localhost:3000'],
-    //   allowed_origins: ['http://localhost:3000'],
-    // })
-    // await installAuth0Roles(['valdas.mazrimas@gmail.com'])
     const pubSub = await initPubSub()
     const result = await bootstrapFederatedServer({
       schemaOpts: {
